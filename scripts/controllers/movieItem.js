bfa14a95676488e925ebe26b90bd63c97c642e08
Eric Mustieles Salvador <eric.mustielessalvador@gmail.com>
2016-09-02 21:18:38 +0200

    Style changes

--- scripts/controllers/movieItem.js
+++ scripts/controllers/movieItem.js
@@ -9,17 +9,17 @@
  */
 angular.module('satrapApp')
 	.controller('MovieItemController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
-	    
+
 		$scope.params = $routeParams;
-	    
+
 	    //Receives the movie ID from the URL parameter
 	    var movieID = $scope.params.movieID;
 
 	    //Makes the ajax call to get all the information from the movie
-	    var ajax_url = 'http://www.omdbapi.com/?i='+movieID+'&plot=short&r=json&tomatoes=true';
+	    var ajax_url = 'http://www.omdbapi.com/?i='+movieID+'&plot=full&r=json&tomatoes=true';
 	      $http.get(ajax_url).success(function(data) {
 	            $scope.movie = data;
 	      });
-	    
-	    
+
+
 	}]);
